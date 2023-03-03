d7dd72c8f39ce992ae3f9cbb6e330aa24d736b40
AZiz <q.aziz@campus.tu-berlin>
2023-03-03 19:02:30 +0100

    First commit.

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
